dd62aca843acd349367ebd6c16f940d54f34dd75
AdmSynap <support@synapsis.com>
2026-02-16 19:30:09 -0300

    alt

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
+// Este ficheiro instrui o PostCSS a usar o Tailwind e o Autoprefixer para processar o CSS.
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
